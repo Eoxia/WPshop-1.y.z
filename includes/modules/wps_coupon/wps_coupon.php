e42008507cb2273bb65e927f28274ccb845dc0c9
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-05-13 23:50:35 +0200

    wps_coupon checked $_POST, $_GET

--- includes/modules/wps_coupon/wps_coupon.php
+++ includes/modules/wps_coupon/wps_coupon.php
@@ -1,17 +1,11 @@
 <?php if ( !defined( 'ABSPATH' ) ) exit;
-/**
- Plugin Name: WPS Coupon
- Description: Manage coupons
- Version: 1.0
- Author: Eoxia
- Author URI: http://eoxia.com/
- */
+
 /**
  * Bootstrap file
  * @author Development team <dev@eoxia.com>
  * @version 1.0
  */
- 
+
 DEFINE('WPS_COUPON_DIR', basename(dirname(__FILE__)));
 DEFINE('WPS_COUPON_PATH', str_replace( "\\", "/", str_replace( WPS_COUPON_DIR, "", dirname( __FILE__ ) ) ) );
 DEFINE('WPS_COUPON_URL', str_replace( str_replace( "\\", "/", ABSPATH), site_url() . '/', WPS_COUPON_PATH ) );
