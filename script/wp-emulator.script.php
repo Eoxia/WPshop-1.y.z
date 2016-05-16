a38c14cdfea31a8a0281c8e61689827d22021f74
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-05-16 11:42:27 +0200

    Update test

--- script/wp-emulator.script.php
+++ script/wp-emulator.script.php
@@ -1,16 +1,16 @@
-<?php if ( !defined( 'ABSPATH' ) ) exit; 
+<?php
 	define('ABSPATH', dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DIRECTORY_SEPARATOR);
 	define('WP_PLUGIN_DIR', dirname(dirname(dirname(__FILE__))) );
 	define('WP_LANG_DIR', 'FR');
 	define('WP_CONTENT_DIR', 'wp-content');
-	
+
 	function __( $str )
 	{
 		$translate = "Result Testing text";
-		echo "[+] Requesting __ -> " . $str . PHP_EOL;	
+		echo "[+] Requesting __ -> " . $str . PHP_EOL;
 		return $translate;
 	}
-	
+
 	function add_action( $init, $object )
 	{
 		$length = 0;
@@ -23,7 +23,7 @@
 			{
 				$length++;
 				$type = gettype($hook);
-				
+
 				switch($type)
 				{
 					case "integer":
@@ -43,7 +43,7 @@
 		else $hooked .= $object;
 		echo "[+] Adding action -> " . $init . " in " . $hooked . PHP_EOL;
 	}
-	
+
 	function register_activation_hook( $file , $object )
 	{
 		$length = 0;
@@ -56,7 +56,7 @@
 			{
 				$length++;
 				$type = gettype($hook);
-				
+
 				switch($type)
 				{
 					case "integer":
@@ -76,7 +76,7 @@
 		else $hooked .= $object;
 		echo "[+] Registering actiovation hook -> " . $file . " in " . $hooked . PHP_EOL;
 	}
-	
+
 	function register_deactivation_hook( $file , $object )
 	{
 		$length = 0;
@@ -89,7 +89,7 @@
 			{
 				$length++;
 				$type = gettype($hook);
-				
+
 				switch($type)
 				{
 					case "integer":
@@ -109,170 +109,170 @@
 		else $hooked .= $object;
 		echo "[+] Registering actiovation hook -> " . $file . " in " . $hooked . PHP_EOL;
 	}
-	
+
 	function add_filter( $name, $fn, $nbr = "")
 	{
-		echo "[+] Adding filter -> " . $name . " X" . $nbr . PHP_EOL;	
+		echo "[+] Adding filter -> " . $name . " X" . $nbr . PHP_EOL;
 	}
-	
+
 	function add_shortcode( $name )
 	{
-		echo "[+] Adding shortcode -> " . $name . PHP_EOL;	
+		echo "[+] Adding shortcode -> " . $name . PHP_EOL;
 	}
-	
+
 	function add_option( $name, $conf)
 	{
 		echo "[+] Adding option -> " . $name . " conf:" . $conf . PHP_EOL;
 	}
-	
+
 	function update_option( $name, $conf)
 	{
 		echo "[+] Updating option -> " . $name . " conf:" . $conf . PHP_EOL;
 	}
-	
+
 	function sanitize_text_field($str)
 	{
-		echo "[+] Sanitizing -> " . $str . PHP_EOL;	
+		echo "[+] Sanitizing -> " . $str . PHP_EOL;
 	}
-	
+
 	function get_posts()
 	{
-		echo "[+] Requesting posts " . PHP_EOL;	
+		echo "[+] Requesting posts " . PHP_EOL;
 	}
-	
+
 	function get_role()
 	{
-		echo "[+] Requesting role " . PHP_EOL;	
+		echo "[+] Requesting role " . PHP_EOL;
 	}
-	
+
 	function load_textdomain()
 	{
-		echo "[+] Loading text domain" . PHP_EOL;	
+		echo "[+] Loading text domain" . PHP_EOL;
 	}
-	
+
 	function plugin_dir_path($path)
 	{
 		echo "[+] Plugin path -> " . $path . PHP_EOL;
 		return trailingslashit( dirname( $path ) );
 	}
-	
-	function trailingslashit( $string ) 
+
+	function trailingslashit( $string )
 	{
 	    return untrailingslashit( $string ) . '/';
 	}
-	
-	function untrailingslashit( $string ) 
+
+	function untrailingslashit( $string )
 	{
 	        return rtrim( $string, '/\\' );
 	}
-	
+
 	function site_url()
 	{
 		$url = "http://testunitaire.dom/";
-		echo "[+] Requesting site_url -> " . $url . PHP_EOL;	
+		echo "[+] Requesting site_url -> " . $url . PHP_EOL;
 		return $url;
 	}
-	
+
 	function plugins_url()
 	{
 		$url = "http://testunitaire.dom/wp-content/plugins/wpshop/";
-		echo "[+] Requesting plugins_url -> " . $url . PHP_EOL;	
+		echo "[+] Requesting plugins_url -> " . $url . PHP_EOL;
 		return $url;
 	}
-	
+
 	function is_multisite()
 	{
 		$result = true;
-		echo "[+] Requesting is_multisite -> " . $result . PHP_EOL;	
+		echo "[+] Requesting is_multisite -> " . $result . PHP_EOL;
 		return $result;
 	}
-	
+
 	function is_admin()
 	{
 		$result = true;
-		echo "[+] Requesting is admin -> " . $result . PHP_EOL;	
+		echo "[+] Requesting is admin -> " . $result . PHP_EOL;
 		return $result;
 	}
-	
+
 	function admin_url()
 	{
 		$url = "http://testunitaire.dom/wp-admin/";
-		echo "[+] Requesting admin_url -> " . $url . PHP_EOL;	
+		echo "[+] Requesting admin_url -> " . $url . PHP_EOL;
 		return $url;
 	}
-	
+
 	function plugin_basename()
 	{
-		$basename = dirname(__FILE__); 
-		echo "[+] Requesting plugin_basename -> " . $basename . PHP_EOL;	
+		$basename = dirname(__FILE__);
+		echo "[+] Requesting plugin_basename -> " . $basename . PHP_EOL;
 		return $basename;
 	}
-	
+
 	function wp_upload_dir()
 	{
 		$dir = dirname(__FILE__);
-		echo "[+] Requesting wp_upload_dir -> " . $dir . PHP_EOL;	
+		echo "[+] Requesting wp_upload_dir -> " . $dir . PHP_EOL;
 		return $dir;
 	}
-	
+
 	function add_theme_support( $theme )
 	{
-		echo "[+] Adding theme support -> " . $theme . PHP_EOL;	
+		echo "[+] Adding theme support -> " . $theme . PHP_EOL;
 	}
-	
+
 	function add_image_size( $image, $x, $y, $bool )
 	{
-		echo "[+] Adding image size -> " . $image . " x:" . $x . " y:" . $y . " bool:" . $bool . PHP_EOL;	
+		echo "[+] Adding image size -> " . $image . " x:" . $x . " y:" . $y . " bool:" . $bool . PHP_EOL;
 	}
-	
+
 	function get_locale()
 	{
 		$lang = "fr-FR";
-		echo "[+] Requesting get_local -> " . $lang . PHP_EOL;	
+		echo "[+] Requesting get_local -> " . $lang . PHP_EOL;
 		return $lang;
 	}
-	
+
 	function load_plugin_textdomain( $name, $bool, $dir)
 	{
-		echo "[+] Loading plugin textdomain -> " . $name . " bool:" . $bool . " dir:" . $dir . PHP_EOL;	
+		echo "[+] Loading plugin textdomain -> " . $name . " bool:" . $bool . " dir:" . $dir . PHP_EOL;
 	}
-	
+
 	function get_option( $name, $int = 0 )
 	{
 		echo "[+] Getting option -> " . $name . " int:" . $int . PHP_EOL;
 		return "";
 	}
-	
+
 	function get_site_option( $name, $int = 0 )
 	{
 		echo "[+] Getting site option -> " . $name . " int:" . $int . PHP_EOL;
 		return "";
 	}
-	
+
 	function current_time( $str )
 	{
 		$time = " 2005-08-05 10:41:13";
-		echo "[+] Requesting current_time -> " . $str . PHP_EOL;	
+		echo "[+] Requesting current_time -> " . $str . PHP_EOL;
 		return $time;
 	}
-	
-	class WPDB 
-	{     
-		function prepare( $request = "" ) 
-		{ 
+
+	class WPDB
+	{
+		function prepare( $request = "" )
+		{
 			echo "[+] Preparing -> " . $request . PHP_EOL;
 		}
-		
-		public static function get_var( $request = "" ) 
-		{ 
+
+		public static function get_var( $request = "" )
+		{
 			echo "[+] Requesting get var -> " . $request . PHP_EOL;
 		}
-		
-		public static function get_charset_collate( $request = "" ) 
-		{ 
+
+		public static function get_charset_collate( $request = "" )
+		{
 			echo "[+] Requesting get charset collate -> " . $request . PHP_EOL;
 		}
-		
+
 		public $usermeta = "usermeta";
 		public $users = "user";
 		public $blogs = "blog";
@@ -287,24 +287,24 @@
 		public $commentmeta = "commentmeta";
 		public $term_relationships = "term_relationship";
 		public $term_taxonomy = "term_taxonomy";
-		
-    } 
-	
+
+    }
+
 	class WP_Widget
 	{
 		function __construct()
 		{
-			
+
 		}
 	}
-	
+
 	class Walker
 	{
 		function __construct()
 		{
-			
+
 		}
 	}
-	
+
 	global $wpdb;
-	$wpdb = new WPDB(); 
+	$wpdb = new WPDB();
