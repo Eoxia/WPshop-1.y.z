3524b47e4abb8e43efa6fc335d81bc1d6a3aadde
Jimmy Latour <jimmy.eoxia@gmail.com>
2016-05-17 11:50:01 +0200

    request.test.php no open request.test.php

--- test/request.test.php
+++ test/request.test.php
@@ -23,7 +23,7 @@ foreach ( $unitList as $file_url )
 		foreach ( $lines as $key => $line ) {
 	    if ( preg_match( $pattern, $line ) ) {
 	      $lines[$key] = preg_replace( '#!empty\(.+?(\$_POST|\$_GET|\$_REQUEST|\$_SESSION)\[\'.+\'\].+?\) \?#isU', '', $lines[$key] );
-			if ( $file_url != "../test/request.test.php" ) {
+			if ( $file_url != "..\\test\\request.test.php" ) {
 			  if ( !preg_match( '#sanitize_.+#', $lines[$key] ) &&
 				!preg_match( '#\*#', $lines[$key] ) &&
 				!preg_match( '#\\/\/#', $lines[$key] ) &&
