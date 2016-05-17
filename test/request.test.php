75a3e77891ea33164935fbb62c2090cdd60f3468
Jimmy Latour <jimmy.eoxia@gmail.com>
2016-05-17 10:11:42 +0200

    Please enter the commit message for your changes. Lines starting

--- test/request.test.php
+++ test/request.test.php
@@ -27,7 +27,8 @@ foreach ( $unitList as $file_url )
 			  if ( !preg_match( '#sanitize_.+#', $lines[$key] ) &&
 				!preg_match( '#\*#', $lines[$key] ) &&
 				!preg_match( '#\\/\/#', $lines[$key] ) &&
-				!preg_match( '#( ?int ?)#', $lines[$key] ) ) {
+				!preg_match( '#( ?int ?)#', $lines[$key] ) && 
+				!preg_match( '#varSanitizer#', $lines[$key] ) ) {
 				  $string_post_unsecured[$file_url][$key + 1] = htmlentities( $lines[$key] );
 				  $total_unsecured_line++;
 			  }
