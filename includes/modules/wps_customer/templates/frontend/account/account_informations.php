304f22e94721fba283b76a73004f7a45bd916189
Alexandre Techer <alexandre.techer@eoxia.com>
2017-03-20 14:41:05 +0100

    * COrrection affichage dans les commandes du lien de connexion en tant que

--- includes/modules/wps_customer/templates/frontend/account/account_informations.php
+++ includes/modules/wps_customer/templates/frontend/account/account_informations.php
@@ -6,6 +6,6 @@
 	<div><button data-nonce="<?php echo wp_create_nonce( 'wps_fill_account_informations_modal' ); ?>" class="wps-link wps-alignright" id="wps_modal_account_informations_opener"><i class="wps-icon-pencil"></i><?php _e( 'Edit your account informations', 'wpshop')?></button></div>
 	<?php elseif( $customer_link ) : ?>
 		<div><a href="<?php echo get_edit_post_link( $cid ); ?>"><?php esc_html_e( 'See customer', 'wpshop' ); ?></a></div>
-		<?php echo apply_filters( 'wps_filter_customer_in_order', $cid, wps_customer_ctr::get_author_id_by_customer_id( $cid ) ); ?>
+		<?php apply_filters( 'wps_filter_customer_in_order', $cid, wps_customer_ctr::get_author_id_by_customer_id( $cid ) ); ?>
 	<?php endif; ?>
 </div>
