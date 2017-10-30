24e097f375f49a9c4d0b35cdb05b11847eaf6695
Alexandre Techer - Eoxia <alexandre.techer@eoxia.com>
2017-10-30 17:09:01 +0100

    1.5.0: Correction de l'export des commandes depuis le tableau de bord

--- includes/modules/wps_export/model/wps_export_mdl.php
+++ includes/modules/wps_export/model/wps_export_mdl.php
@@ -120,47 +120,57 @@ class wps_export_mdl {
 			'order_discount_amount' => __( 'Order discount', 'wps_export' ),
 			'order_grand_total' => __( 'Order ATI', 'wps_export' ),
 		);
-		$orders = get_posts( array(
-			'post_type' => WPSHOP_NEWTYPE_IDENTIFIER_ORDER,
+		$query_args = array(
+			'post_type'      => WPSHOP_NEWTYPE_IDENTIFIER_ORDER,
 			'posts_per_page' => -1,
-		) );
-		if ( ! empty( $orders ) ) {
-			foreach ( $orders as $order ) {
-				if ( ! empty( $dt1 ) && strtotime( $dt1 ) <= strtotime( $order->post_date ) && strtotime( $order->post_date ) <= strtotime( '+1 day', strtotime( $dt2 ) ) ) {
-					$user = get_userdata( $order->post_author );
-					$tmp_array = array();
-					$order_postmeta = get_post_meta( $order->ID, '_order_postmeta', true );
-					$tmp_array['order_type'] = ( !empty($order_postmeta['order_invoice_ref']) ) ? __( 'Invoice', 'wps_export' ) : ( ( !empty($order_postmeta['order_key']) ) ? __( 'Order', 'wps_export' ) : __( 'Quotation', 'wps_export' ) );
-					$tmp_array['order_invoice_ref'] = ( !empty($order_postmeta['order_invoice_ref']) ) ? $order_postmeta['order_invoice_ref'] : ( ( !empty($order_postmeta['order_key']) ) ? $order_postmeta['order_key'] : $order_postmeta['order_temporary_key'] );
-					$tmp_array['name'] = ( !empty($user->ID) ) ? get_user_meta( $user->ID, 'last_name', true ) : '';
-					$tmp_array['first_name'] = ( !empty($user->ID) ) ? get_user_meta( $user->ID, 'first_name', true ) : '';
-					$tmp_array['email'] = ( !empty($user->user_email) ) ? $user->user_email : '';
-					$tmp_array['tel'] = '';
-					$order_info = get_post_meta( $order->ID, '_order_info', true);
-					if( !empty($order_info) && !empty($order_info['billing']['address']['phone']) ) {
-						$tmp_array['tel'] .= $order_info['billing']['address']['phone'];
-					}
-					$tmp_array['date_order'] = '';
-					if( !empty($order->post_date) ) {
-						$tmp_array['date_order'] = mysql2date( get_option( 'date_format' ), $order->post_date, true );
-					}
-					$tmp_array['order_total_et'] = number_format( ( !empty($order_postmeta['order_total_ht']) ) ? $order_postmeta['order_total_ht'] : 0, 2, '.', '' );
-					//$tmp_array['order_total_ati'] = number_format( ( !empty($order_postmeta['order_total_ttc']) ) ? $order_postmeta['order_total_ttc'] : 0, 2, '.', '' );
-					$order_shipping_cost = ( !empty($order_postmeta['order_shipping_cost']) ) ? $order_postmeta['order_shipping_cost'] : 0;
-					$price_piloting_option = get_option( 'wpshop_shop_price_piloting' );
-					if( !empty($price_piloting_option) && $price_piloting_option == 'HT' ) {
-						$shipati = $order_shipping_cost * 1.2;
-						$shipet = $order_shipping_cost;
-					} else {
-						$shipet = $order_shipping_cost / 1.2;
-						$shipati = $order_shipping_cost;
-					}
-					$tmp_array['order_shipping_cost_et'] = number_format( $shipet, 2, '.', '' );
-					$tmp_array['order_shipping_cost_ati'] = number_format( $shipati, 2, '.', '' );
-					$tmp_array['order_discount_amount'] = number_format( ( !empty($order_postmeta['order_discount_amount_total_cart']) ) ? $order_postmeta['order_discount_amount_total_cart'] : 0, 2, '.', '' );
-					$tmp_array['order_grand_total'] = number_format( ( !empty($order_postmeta['order_grand_total']) ) ? $order_postmeta['order_grand_total'] : 0, 2, '.', '' );
-					$commands_array[] = $tmp_array;
+		);
+
+		$date_query = array();
+		if ( null !== $dt1 ) {
+			$date_query['after'] = $dt1;
+		}
+		if ( null !== $dt2 ) {
+			$date_query['before'] = $dt2;
+		}
+		if ( ! empty( $date_query ) ) {
+			$query_args['date_query'] = wp_parse_args( $date_query, array( 'inclusive' => true ) );
+		}
+		$orders = new WP_Query( $query_args );
+		if ( $orders->have_posts() ) {
+			foreach ( $orders->posts as $order ) {
+				$user = get_userdata( $order->post_author );
+				$tmp_array = array();
+				$order_postmeta = get_post_meta( $order->ID, '_order_postmeta', true );
+				$tmp_array['order_type'] = ( !empty($order_postmeta['order_invoice_ref']) ) ? __( 'Invoice', 'wps_export' ) : ( ( !empty($order_postmeta['order_key']) ) ? __( 'Order', 'wps_export' ) : __( 'Quotation', 'wps_export' ) );
+				$tmp_array['order_invoice_ref'] = ( !empty($order_postmeta['order_invoice_ref']) ) ? $order_postmeta['order_invoice_ref'] : ( ( !empty($order_postmeta['order_key']) ) ? $order_postmeta['order_key'] : $order_postmeta['order_temporary_key'] );
+				$tmp_array['name'] = ( !empty($user->ID) ) ? get_user_meta( $user->ID, 'last_name', true ) : '';
+				$tmp_array['first_name'] = ( !empty($user->ID) ) ? get_user_meta( $user->ID, 'first_name', true ) : '';
+				$tmp_array['email'] = ( !empty($user->user_email) ) ? $user->user_email : '';
+				$tmp_array['tel'] = '';
+				$order_info = get_post_meta( $order->ID, '_order_info', true);
+				if( !empty($order_info) && !empty($order_info['billing']['address']['phone']) ) {
+					$tmp_array['tel'] .= $order_info['billing']['address']['phone'];
 				}
+				$tmp_array['date_order'] = '';
+				if( !empty($order->post_date) ) {
+					$tmp_array['date_order'] = mysql2date( get_option( 'date_format' ), $order->post_date, true );
+				}
+				$tmp_array['order_total_et'] = number_format( ( !empty($order_postmeta['order_total_ht']) ) ? $order_postmeta['order_total_ht'] : 0, 2, '.', '' );
+				//$tmp_array['order_total_ati'] = number_format( ( !empty($order_postmeta['order_total_ttc']) ) ? $order_postmeta['order_total_ttc'] : 0, 2, '.', '' );
+				$order_shipping_cost = ( !empty($order_postmeta['order_shipping_cost']) ) ? $order_postmeta['order_shipping_cost'] : 0;
+				$price_piloting_option = get_option( 'wpshop_shop_price_piloting' );
+				if( !empty($price_piloting_option) && $price_piloting_option == 'HT' ) {
+					$shipati = $order_shipping_cost * 1.2;
+					$shipet = $order_shipping_cost;
+				} else {
+					$shipet = $order_shipping_cost / 1.2;
+					$shipati = $order_shipping_cost;
+				}
+				$tmp_array['order_shipping_cost_et'] = number_format( $shipet, 2, '.', '' );
+				$tmp_array['order_shipping_cost_ati'] = number_format( $shipati, 2, '.', '' );
+				$tmp_array['order_discount_amount'] = number_format( ( !empty($order_postmeta['order_discount_amount_total_cart']) ) ? $order_postmeta['order_discount_amount_total_cart'] : 0, 2, '.', '' );
+				$tmp_array['order_grand_total'] = number_format( ( !empty($order_postmeta['order_grand_total']) ) ? $order_postmeta['order_grand_total'] : 0, 2, '.', '' );
+				$commands_array[] = $tmp_array;
 			}
 		}
 		$commands_array = apply_filters( 'wps_order_export_filter', $commands_array, $orders, $dt1, $dt2 );
